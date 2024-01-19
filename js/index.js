35e68569faff68257068518de20f9b598b207f48
Melissa Martindale <mmartindale3@nait.ca>
2024-01-18 21:59:42 -0700

    Fixed js script linking. Figured out code

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
+console.log("javascript starts running");
+
 const profileForm = document.querySelector("#profile-form");
 // console.log(profileForm);
-
 profileForm.addEventListener("submit", onUpdateProfile);
 
- 
+
 function onUpdateProfile(e) {
     e.preventDefault();
     const firstName = document.querySelector("#first-name").value.trim();
     const lastName = document.querySelector("#last-name").value.trim();
-    console.log(firstName, lastName)
+    const fullNameDisplay = document.querySelector("#full-name");
+    fullNameDisplay.textContent = `${firstName} ${lastName}`;
+    console.log(fullNameDisplay);
 }
